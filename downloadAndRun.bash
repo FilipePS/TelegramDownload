7aae57b4a7d7fc5ca479e5907dd10f8f4fec6484
FilipePS <filipe.panni@gmail.com>
2022-01-10 21:50:07 -0300

    Script baixar/executar automaticamente

---
+++ downloadAndRun.bash
@@ -0,0 +1,6 @@
+sudo apt update
+sudo apt install git
+git clone https://github.com/FilipePS/TelegramDownload
+cd TelegramDownload
+./install.bash
+python3 telegramDownload.py
---
+++ readme.md
@@ -0,0 +1,1 @@
+`wget -O - https://raw.githubusercontent.com/FilipePS/TelegramDownload/master/downloadAndRun.bash | bash`
